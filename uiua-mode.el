2d13b45dfb8ab0ba56cad1e775e2f70c256b4bee
Andrew Jose <arnav.jose@gmail.com>
2023-11-09 23:21:01 +0530

    main file boilerplate

---
+++ uiua-mode.el
@@ -0,0 +1,18 @@
+;;; uiua-mode.el --- Uiua integration for Emacs -*- lexical-binding:t -*-
+;;; Version: 0.0.1
+
+;; SPDX-License-Identifier: GPL-3.0-or-later
+;; URL: https://github.com/crmsnbleyd/uiua-mode
+;; Package-Requires: ((emacs "27.1"))
+;; Keywords: languages, uiua
+
+;;; Commentary:
+;; `uiua-mode' is an emacs major mode for interacting with
+;; and editing the uiua array language
+
+;;; Code:
+(message "Nothing yet")
+
+(provide 'uiua-mode)
+
+;;; uiua-mode.el ends here
